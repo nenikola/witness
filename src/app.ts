7feb60a75c55fe435f35e7b139795c1ecaf6d18b
nenikola <nenikola.dev@gmail.com>
2022-03-31 00:06:06 +0200

    use port number from env

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ app.post(
   makeExpressCallback(reportController.postReport)
 );
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
 });
